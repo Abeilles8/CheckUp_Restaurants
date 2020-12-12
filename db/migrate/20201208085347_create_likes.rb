5acc941678e2f4898e0213ce5d2e33f4e1eb5c3a
EC2 Default User <ec2-user@ip-172-31-48-97.ec2.internal>
2020-12-12 15:56:47 +0000

    update db

--- db/migrate/20201208085347_create_likes.rb
+++ db/migrate/20201208085347_create_likes.rb
@@ -1,8 +1,13 @@
 class CreateLikes < ActiveRecord::Migration[5.2]
   def change
     create_table :likes do |t|
-
+      t.references :user, foreign_key: true
+      t.references :review, foreign_key: true
+      
       t.timestamps
+      
+      # unique制約で重複を防ぐ
+      t.index [:user_id, :review_id], unique: true
     end
   end
 end
